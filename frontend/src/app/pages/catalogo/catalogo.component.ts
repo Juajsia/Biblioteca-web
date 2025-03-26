fefe39715272f6a52e968092c69c215a6a68cf53
Juan Adams <juan_adams82212@elpoli.edu.co>
2025-03-25 23:33:14 -0500

    getall books and authors

--- frontend/src/app/pages/catalogo/catalogo.component.ts
+++ frontend/src/app/pages/catalogo/catalogo.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterModule } from '@angular/router';
+import { BookService } from '../../services/book.service';
+import { AuthorService } from '../../services/author.service';
 
 @Component({
   selector: 'app-catalogo',
@@ -14,6 +16,23 @@ export class CatalogoComponent {
   showAuthorForm = false
   userType = localStorage.getItem('type')
 
+  books: any = []
+  authors: any = []
+
+  constructor(private _bookService: BookService, private _authorService: AuthorService) {
+    this.getbooks()
+    this.getauthors()
+  }
+
+  search() {
+  }
+  getbooks() {
+    this._bookService.findAll().subscribe(data => { this.books = data })
+  }
+
+  getauthors() {
+    this._authorService.findAll().subscribe(data => { this.authors = data })
+  }
   logOut() {
     localStorage.clear()
   }
